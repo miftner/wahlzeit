bf119c9de12296cafcc56fa048ccda2fbf59bc2a
Maximilian Iftner <maximilian.iftner@fau.de>
2016-11-13 21:47:44 +0100

    Added LiquorPhotoFactory Class

---
+++ src/main/java/org/wahlzeit/model/LiquorPhotoFactory.java
@@ -0,0 +1,22 @@
+package org.wahlzeit.model;
+
+public class LiquorPhotoFactory extends PhotoFactory{
+
+    /**
+     * Make instance of factory accessible without exposing the constructor
+     * @return
+     */
+    public static LiquorPhotoFactory getFactory(){
+        return instance;
+    }
+
+    private static LiquorPhotoFactory instance = new LiquorPhotoFactory();
+
+    public Photo createPhoto(){
+        return new LiquorPhoto();
+    }
+
+    public LiquorPhoto createPhoto(PhotoId id) {
+        return new LiquorPhoto(id);
+    }
+}
