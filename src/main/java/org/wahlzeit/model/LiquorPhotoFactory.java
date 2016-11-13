3748310d7b76e1acf8a9737708cf283e927c671f
Maximilian Iftner <maximilian.iftner@fau.de>
2016-11-13 22:48:06 +0100

    Fixed Typo

--- src/main/java/org/wahlzeit/model/LiquorPhotoFactory.java
+++ src/main/java/org/wahlzeit/model/LiquorPhotoFactory.java
@@ -6,11 +6,12 @@ public class LiquorPhotoFactory extends PhotoFactory{
      * Make instance of factory accessible without exposing the constructor
      * @return
      */
-    public static LiquorPhotoFactory getFactory(){
+    public static LiquorPhotoFactory getInstance(){
         return instance;
     }
 
     private static LiquorPhotoFactory instance = new LiquorPhotoFactory();
+
 
     public Photo createPhoto(){
         return new LiquorPhoto();
