3c1b31ef78a42e817caae5391abcdf44e4755c03
Maximilian Iftner <maximilian.iftner@fau.de>
2016-11-13 21:39:56 +0100

    Added LiquorPhoto Class

---
+++ src/main/java/org/wahlzeit/model/LiquorPhoto.java
@@ -0,0 +1,43 @@
+package org.wahlzeit.model;
+
+
+
+public class LiquorPhoto extends Photo {
+
+protected String characteristic;
+
+ /**
+  * Get Default id from system and pipe it to constructor
+  */
+ public LiquorPhoto(){
+  this(PhotoId.getNextId(), null);
+ }
+
+ /**
+  * Set custom id but no characteristic
+  * @param id
+  */
+ public LiquorPhoto(PhotoId id){
+  this(id, null);
+ }
+
+ /**
+  * Default Constructor
+  * @param id
+  * @param newCharacteristic
+  */
+ public LiquorPhoto(PhotoId id, String newCharacteristic){
+  super(id);
+  this.characteristic = newCharacteristic;
+ }
+
+
+ public void setCharacteristic(String newChar){
+  this.characteristic = newChar;
+ }
+
+ public String getCharacteristic(){
+  return this.characteristic;
+ }
+
+}
